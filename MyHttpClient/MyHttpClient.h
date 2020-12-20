04614f8574c6dcc040e1cbe184dc6b83cbcf2f8f
base0 <vacapp@live.com>
2020-12-21 05:59:32 +0700

    add getHttpCode()

--- MyHttpClient/MyHttpClient.h
+++ MyHttpClient/MyHttpClient.h
@@ -3,21 +3,19 @@
 #define MY_HTTP_CLIENT
 
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
-
 #include <WiFiClient.h>
 
-
 class MyHttpClient {
-  public:
-  void setup(char*, char*);
-  int get(char* url);
+public:
+  void setup(const char*, const char*);
+  int get(const char* url);
+  int getHttpCode();
   String& getResponse();
-  private:
+private:
+  int httpCode;
   String response;
 };
 
