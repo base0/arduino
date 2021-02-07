a4f7cff3fac5da4f2547baa02b9f1169dbb0800f
base0 <vacapp@live.com>
2021-02-08 06:35:07 +0700

    Create 1 button.ino

---
+++ blynk/1 button.ino
@@ -0,0 +1,23 @@
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
+
+BLYNK_WRITE(V3) {
+  int pinValue = param.asInt();
+  Serial.println(pinValue);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop() {
+  Blynk.run();
+}
